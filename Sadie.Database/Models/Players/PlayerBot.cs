34a8150caf6a57dc47ceab5c7776f609b8670dcf
habtard <habtard@gmail.com>
2025-04-19 17:03:00 +0100

    Initial commit

---
+++ Sadie.Database/Models/Players/PlayerBot.cs
@@ -0,0 +1,16 @@
+using Sadie.Enums.Unsorted;
+
+namespace Sadie.Database.Models.Players;
+
+public class PlayerBot
+{
+    public int Id { get; init; }
+    public required long PlayerId { get; init; }
+    public required int? RoomId { get; set; }
+    public required string Username { get; init; }
+    public required string FigureCode { get; init; }
+    public required string Motto { get; init; }
+    public required AvatarGender Gender { get; init; }
+    public ChatBubble ChatBubbleId { get; init; }
+    public required DateTime CreatedAt { get; init; }
+}
---
+++ Sadie.Database/Models/Rooms/RoomTag.cs
@@ -0,0 +1,7 @@
+namespace Sadie.Database.Models.Rooms;
+
+public class RoomTag
+{
+    public int Id { get; init; }
+    public required string Name { get; init; }
+}
