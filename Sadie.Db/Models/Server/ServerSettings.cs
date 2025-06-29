6fb85254c62f52be4d17e3b4937f5e830884a09e
habtard <habtard@gmail.com>
2025-06-29 08:34:56 +0100

    updates

--- Sadie.Db/Models/Server/ServerSettings.cs
+++ Sadie.Db/Models/Server/ServerSettings.cs
@@ -2,8 +2,7 @@ namespace Sadie.Db.Models.Server;
 
 public class ServerSettings
 {
-    public int Id { get; set; }
-    
+    public int Id { get; init; }
     public string? PlayerWelcomeMessage { get; init; }
     public bool FairCurrencyRewards { get; init; }
 }
