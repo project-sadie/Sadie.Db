85bf9c6989ceb5c10d2e47d2bcc94cd8191335a1
habtard <habtard@gmail.com>
2025-07-21 18:55:04 +0100

    removed id

--- Sadie.Db/Models/Server/ServerSettings.cs
+++ Sadie.Db/Models/Server/ServerSettings.cs
@@ -2,7 +2,6 @@ namespace Sadie.Db.Models.Server;
 
 public class ServerSettings
 {
-    public int Id { get; init; }
     public string? PlayerWelcomeMessage { get; init; }
     public bool FairCurrencyRewards { get; init; }
 }
