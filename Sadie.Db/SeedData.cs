ab30e66c0f025f90e85e289db7ccb726d2792f31
habtard <habtard@gmail.com>
2025-07-26 19:52:05 +0100

    fixes

--- Sadie.Db/SeedData.cs
+++ Sadie.Db/SeedData.cs
@@ -7,7 +7,7 @@ public static class SeedData
     public static async Task SeedInitialDataAsync(SadieDbContext dbContext)
     {
         using var httpClient = new HttpClient();
-        var initialSql = await httpClient.GetStringAsync("https://cdn.sadie.pw/initialData.sql");
+        var initialSql = await httpClient.GetStringAsync("https://cdn.sadie.pw/seed.sql");
         await dbContext.Database.ExecuteSqlRawAsync(initialSql);
     }
 }
