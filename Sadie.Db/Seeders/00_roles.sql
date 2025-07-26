08f9c489fcbe62004999cf7611034ea637a8f7c1
habtard <habtard@gmail.com>
2025-07-26 17:31:36 +0100

    cleanup

--- Sadie.Db/Seeders/00_roles.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `roles` (`id`, `name`) VALUES
-(1, 'User'),
-(5, 'Moderator'),
-(6, 'Admin');
--- Sadie.Db/Seeders/00_server_player_constants.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `server_player_constants` (
-    `max_motto_length`, `min_sso_length`, `max_friendships`, `created_at`
-) VALUES
-(30, 8, 5000, NOW());
